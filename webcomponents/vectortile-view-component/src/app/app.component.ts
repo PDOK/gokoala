7affd3ffe95bef3865df02a1ffda3566dc61ceae
john.schaap@kadaster.nl <John.Schaap@Kadaster.nl>
2023-07-13 16:28:15 +0200

    Change zoom when ogc api url (projection) is changed.

--- webcomponents/vectortile-view-component/src/app/app.component.ts
+++ webcomponents/vectortile-view-component/src/app/app.component.ts
@@ -6,50 +6,50 @@ import {
   Output,
   EventEmitter,
   CUSTOM_ELEMENTS_SCHEMA
-} from '@angular/core';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { Subject } from 'rxjs';
-import { ObjectInfoComponent } from './object-info/object-info.component';
-import VectorTileSource from 'ol/source/VectorTile.js';
-import TileDebug from 'ol/source/TileDebug.js';
-import Map from 'ol/Map';
-import View from 'ol/View';
+} from '@angular/core'
+import { coerceBooleanProperty } from '@angular/cdk/coercion'
+import { Subject } from 'rxjs'
+import { ObjectInfoComponent } from './object-info/object-info.component'
+import VectorTileSource from 'ol/source/VectorTile.js'
+import TileDebug from 'ol/source/TileDebug.js'
+import Map from 'ol/Map'
+import View from 'ol/View'
 import { EuropeanETRS89_GRS80, MapProjection, NetherlandsRDNewQuadDefault } from '../app/mapprojection'
 
-import { FullScreen, defaults as defaultControls } from 'ol/control.js';
-import { applyStyle } from 'ol-mapbox-style';
-import Projection from 'ol/proj/Projection';
-import { Fill, Stroke, Style } from "ol/style";
-import { MVT } from "ol/format";
-import VectorTileLayer from 'ol/layer/VectorTile';
-import { getTopLeft, getWidth } from 'ol/extent';
-import TileGrid from 'ol/tilegrid/TileGrid';
-import { ProjectionLike, useGeographic } from 'ol/proj';
-import { Coordinate } from 'ol/coordinate';
-import TileLayer from 'ol/layer/Tile';
-import BaseLayer from 'ol/layer/Base';
-import Collection from 'ol/Collection';
-import LayerGroup from 'ol/layer/Group';
-import { FeatureLike } from 'ol/Feature';
-import { CommonModule } from '@angular/common';
-import { MatrixsetService } from './matrixset.service';
+import { FullScreen, defaults as defaultControls } from 'ol/control.js'
+import { applyStyle } from 'ol-mapbox-style'
+import Projection from 'ol/proj/Projection'
+import { Fill, Stroke, Style } from "ol/style"
+import { MVT } from "ol/format"
+import VectorTileLayer from 'ol/layer/VectorTile'
+import { getTopLeft, getWidth } from 'ol/extent'
+import TileGrid from 'ol/tilegrid/TileGrid'
+import { ProjectionLike, useGeographic } from 'ol/proj'
+import { Coordinate } from 'ol/coordinate'
+import TileLayer from 'ol/layer/Tile'
+import BaseLayer from 'ol/layer/Base'
+import Collection from 'ol/Collection'
+import LayerGroup from 'ol/layer/Group'
+import { FeatureLike } from 'ol/Feature'
+import { CommonModule } from '@angular/common'
+import { Link, Matrix, MatrixsetService } from './matrixset.service'
 
 
 export type NgChanges<Component extends object, Props = ExcludeFunctions<Component>> = {
   [Key in keyof Props]: {
-    previousValue: Props[Key];
-    currentValue: Props[Key];
-    firstChange: boolean;
-    isFirstChange(): boolean;
+    previousValue: Props[Key]
+    currentValue: Props[Key]
+    firstChange: boolean
+    isFirstChange(): boolean
   }
 }
 
 type MarkFunctionPropertyNames<Component> = {
-  [Key in keyof Component]: Component[Key] extends Function | Subject<any> ? never : Key;
+  [Key in keyof Component]: Component[Key] extends Function | Subject<any> ? never : Key
 }
 
-type ExcludeFunctionPropertyNames<T extends object> = MarkFunctionPropertyNames<T>[keyof T];
-type ExcludeFunctions<T extends object> = Pick<T, ExcludeFunctionPropertyNames<T>>;
+type ExcludeFunctionPropertyNames<T extends object> = MarkFunctionPropertyNames<T>[keyof T]
+type ExcludeFunctions<T extends object> = Pick<T, ExcludeFunctionPropertyNames<T>>
 
 @Component({
   selector: 'app-vectortile-view',
@@ -64,27 +64,27 @@ type ExcludeFunctions<T extends object> = Pick<T, ExcludeFunctionPropertyNames<T
 
 })
 export class
-  AppComponent implements OnInit {
+  AppComponent {
   title = 'vectortile-view-component';
   map = new Map({});
   selector = '/{z}/{y}/{x}?f=mvt'
   private _showGrid: boolean = false;
   private _showObjectInfo: boolean = false;
-  vectorTileLayer!: VectorTileLayer;
-  curFeature!: FeatureLike;
-  tileGrid: TileGrid | undefined;
-  minZoom?: number;
-  maxZoom?: number;
+  vectorTileLayer!: VectorTileLayer
+  curFeature!: FeatureLike
+  tileGrid: TileGrid | undefined
+  minZoom?: number
+  maxZoom?: number
 
   @Input() set showGrid(showGrid: any) {
-    this._showGrid = coerceBooleanProperty(showGrid);
+    this._showGrid = coerceBooleanProperty(showGrid)
   }
   get showGrid() {
     return this._showGrid
   }
 
   @Input() set showObjectInfo(showObjectInfo: any) {
-    this._showObjectInfo = coerceBooleanProperty(showObjectInfo);
+    this._showObjectInfo = coerceBooleanProperty(showObjectInfo)
   }
   get showObjectInfo() {
     return this._showObjectInfo
@@ -93,9 +93,9 @@ export class
   @Input() tileUrl: string = NetherlandsRDNewQuadDefault
   @Input() styleUrl!: string
   @Input() id!: string | undefined
-  @Input() zoom!: number
-  @Input() centerX!: number;
-  @Input() centerY!: number;
+  @Input() zoom!: number | undefined
+  @Input() centerX!: number
+  @Input() centerY!: number
   mapHeight: number = 600
   mapWidth: number = 800
 
@@ -107,183 +107,182 @@ export class
   ngOnChanges(changes: NgChanges<AppComponent>) {
     if (changes.styleUrl?.previousValue !== changes.styleUrl?.currentValue) {
       //console.log(this.id + ' style changed')
-      if (this.vectorTileLayer) {
-        this.setStyle(this.vectorTileLayer);
+      if (!changes.styleUrl.isFirstChange()) {
+        if (this.vectorTileLayer) {
+          this.setStyle(this.vectorTileLayer)
+        }
       }
+
     }
     if (changes.tileUrl?.previousValue !== changes.tileUrl?.currentValue) {
+
       //console.log(this.id + ' projection changed')
-      if (this.vectorTileLayer) {
-        this.setNewProjection();
+      if (changes.tileUrl.isFirstChange()) {
+        this.checkParams()
+      } else {
+        this.zoom = undefined
+        this.maxZoom = undefined
+        this.minZoom = undefined
+
+
+
       }
+      this.initialize()
+
     }
   }
 
-  ngOnInit() {
-    this.checkParams();
-    let matrixurl = this.tileUrl.replace("tiles", "tileMatrixSets") + '?f=json';
+  private initialize() {
+    let matrixurl = this.tileUrl.replace("tiles", "tileMatrixSets") + '?f=json'
     console.log("url: " + this.tileUrl)
     this.matrixsetService.getMatrix(this.tileUrl).subscribe({
       next: tile => {
-
-        tile.links.forEach(link => {
-
-          if (link.rel == 'http://www.opengis.net/def/rel/ogc/1.0/tiling-scheme') {
-            console.log(this.id + " url for matrix: " + link.href)
-            let turl = new URL(this.tileUrl)
-
-            if (isFullURL(link.href)) {
-              matrixurl = link.href
-            }
-            else {
-              let mUrl = new URL(turl.origin + link.href)
-              matrixurl = mUrl.href
-            }
-          }
-        })
-
-        tile.tileMatrixSetLimits.forEach(limit => {
-
-
-          if (!this.zoom) {
-            this.zoom = parseFloat(limit.tileMatrix) + 1
-          }
-          // Only show available tiles
-          if (!this.minZoom) {
-
-            this.minZoom = parseFloat(limit.tileMatrix) + 1
-          }
-          this.maxZoom = parseFloat(limit.tileMatrix) + 1
-        })
-        this.matrixsetService.getMatrixSet(matrixurl).subscribe({
-          next: matrixset => {
-            let resolutions: number[] = [];
-            let origins: number[][] = []
-            let sizes: number[][] = []
-            matrixset.tileMatrices.forEach(x => {
-              resolutions[x.id] = x.cellSize
-
-              if (this.tileUrl.includes(EuropeanETRS89_GRS80)) {
-                origins[x.id] = [x.pointOfOrigin[1], x.pointOfOrigin[0]] //  x,y swap Workaround? 
-              }
-              else {
-                origins[x.id] = x.pointOfOrigin
-              }
-              sizes[x.id] = [x.tileWidth, x.tileHeight]
-            })
-           
-            this.tileGrid = new TileGrid({
-              resolutions: resolutions,
-              tileSizes: sizes,
-              origins: origins
-            })
-            this.drawMap();
-          },
-          error: error => {
-            console.log(this.id + 'tilematrixset not found: ' + matrixurl)
-            const proj = new MapProjection(this.tileUrl).Projection
-            this.tileGrid = new TileGrid({
-              extent: proj.getExtent(),
-              resolutions: this.calcResolutions(proj),
-              tileSize: [256, 256],
-              origin: getTopLeft(proj.getExtent())
-            })
-            this.drawMap();
-
-          }
-        })
-
-
-
-
-
-
-
-
+        const linkurl = this.FindMatrixUrl(tile.links)
+        if (linkurl) {
+          matrixurl = linkurl
+        }
+        this.SetZoom(tile)
+        this.drawFromMatrixUrl(matrixurl)
       },
       error: msg => {
-
         console.log(this.id + "error: " + JSON.stringify(msg))
       }
-
     })
-
-
-    function isFullURL(url: string): boolean {
-      return url.toLowerCase().startsWith('http://') || url.toLowerCase().startsWith('https://');
-    }
-
-
-    //console.log("surl:" + JSON.stringify(this.styleUrl))
   }
 
+  private SetZoom(tile: Matrix) {
+    tile.tileMatrixSetLimits.forEach(limit => {
+      if (!this.zoom) {
+        this.zoom = parseFloat(limit.tileMatrix) + 1
+      }
+      // Only show available tiles
+      if (!this.minZoom) {
+        this.minZoom = parseFloat(limit.tileMatrix) + 1
+      }
+      this.maxZoom = parseFloat(limit.tileMatrix) + 1
+    })
+  }
 
+  private FindMatrixUrl(links: Link[]) {
+    let matrixurl = undefined
+    links.forEach(link => {
+      if (link.rel == 'http://www.opengis.net/def/rel/ogc/1.0/tiling-scheme') {
+        console.log(this.id + " url for matrix: " + link.href)
+        let turl = new URL(this.tileUrl)
+        if (this.isFullURL(link.href)) {
+          matrixurl = link.href
+        }
+        else {
+          let mUrl = new URL(turl.origin + link.href)
+          matrixurl = mUrl.href
+        }
+      }
+    })
+    return matrixurl
+  }
 
+  private drawFromMatrixUrl(matrixurl: string) {
+    this.matrixsetService.getMatrixSet(matrixurl).subscribe({
+      next: matrixset => {
+        let resolutions: number[] = []
+        let origins: number[][] = []
+        let sizes: number[][] = []
+        matrixset.tileMatrices.forEach(x => {
+          resolutions[x.id] = x.cellSize
+
+          if (this.tileUrl.includes(EuropeanETRS89_GRS80)) {
+            origins[x.id] = [x.pointOfOrigin[1], x.pointOfOrigin[0]] //  x,y swap Workaround? 
+          }
+          else {
+            origins[x.id] = x.pointOfOrigin
+          }
+          sizes[x.id] = [x.tileWidth, x.tileHeight]
+        })
+
+        this.tileGrid = new TileGrid({
+          resolutions: resolutions,
+          tileSizes: sizes,
+          origins: origins
+        })
+        this.drawMap()
+      },
+      error: error => {
+        console.log(this.id + 'tilematrixset not found: ' + matrixurl)
+        const proj = new MapProjection(this.tileUrl).Projection
+        this.tileGrid = new TileGrid({
+          extent: proj.getExtent(),
+          resolutions: this.calcResolutions(proj),
+          tileSize: [256, 256],
+          origin: getTopLeft(proj.getExtent())
+        })
+        this.drawMap()
+      }
+    })
+  }
 
   private drawMap() {
-    this.map = this.getMap();
-    this.map.on('pointermove', (evt: { pixel: any; }) => {
+
+    this.map.setTarget(undefined)
+    this.map = this.getMap()
+    this.map.on('pointermove', (evt: { pixel: any }) => {
       this.map.forEachFeatureAtPixel(evt.pixel, (feature: FeatureLike) => {
         if (feature) {
           if (this._showObjectInfo) {
-            this.curFeature = feature;
-            //this.setSelectStyle(this.curFeature)
+            this.curFeature = feature
+          
           }
-          this.activeFeature.emit(feature);
-        //  console.log("resolution: " + this.map.getView().getResolution())
-
+          this.activeFeature.emit(feature)
+      
         }
       },
         { hitTolerance: 3 }
 
-      );
-    });
+      )
+    })
 
-    const mapdiv: HTMLElement = this.elementRef.nativeElement.querySelector("[id='map']");
-    //console.log('mapheight' + this.elementRef.nativeElement.offsetHeight)  //<<<===here
-    //console.log('mapwidth' + this.elementRef.nativeElement.offsetWidth)
-    //if ()
-     this.mapWidth =  this.elementRef.nativeElement.offsetWidth ;
-     this.mapHeight= this.elementRef.nativeElement.offsetHeight
- 
-    this.map.setTarget(mapdiv);
+    const mapdiv: HTMLElement = this.elementRef.nativeElement.querySelector("[id='map']")
+
+    this.mapWidth = this.elementRef.nativeElement.offsetWidth
+    this.mapHeight = this.elementRef.nativeElement.offsetHeight
+    if (!this.map.getTarget()) {
+      this.map.setTarget(mapdiv)
+
+    }
   }
 
   private checkParams(): void {
     console.log(this.id)
     if (!this.tileUrl) {
-      console.error("No TilteUrl was provided for the app-vectortile-view");
+      console.error("No TilteUrl was provided for the app-vectortile-view")
     }
     if (!this.styleUrl) {
-      console.log("No StyleUrl was provided for the app-vectortile-view");
+      console.log("No StyleUrl was provided for the app-vectortile-view")
     }
     if (!this.zoom) {
       //  console.error("No zoom was provided for the app-vectortile-view");
     }
     else
-      console.log("zoom=" + this.zoom);
+      console.log("zoom=" + this.zoom)
     if (!this.centerX) {
-      console.error("No zoom center-x was provided for the app-vectortile-view");
+      console.error("No zoom center-x was provided for the app-vectortile-view")
     }
     else
-      console.log("center-x=" + this.centerX);
+      console.log("center-x=" + this.centerX)
     if (!this.centerY) {
-      console.error("No center-y was provided for the app-vectortile-view");
+      console.error("No center-y was provided for the app-vectortile-view")
     }
     else
-      console.log("center-y=" + this.centerY);
+      console.log("center-y=" + this.centerY)
 
   }
 
   getMap() {
-    useGeographic();
+    useGeographic()
 
-    let layers = this.generateLayers();
+    let layers = this.generateLayers()
 
     let acenter: Coordinate = [this.centerX, this.centerY]
-    //console.log("project " + JSON.stringify(this.vectorTileLayer.getSource()?.getProjection()))
-    //console.log("axis: " + this.vectorTileLayer.getSource()?.getProjection()?.getAxisOrientation())
-    //console.log("acenter=" + acenter)
+  
     return new Map({
       controls: defaultControls().extend([new FullScreen()]),
 
@@ -296,15 +295,15 @@ export class
         enableRotation: false,
         projection: this.vectorTileLayer.getSource()?.getProjection() as ProjectionLike,
       }),
-    });
+    })
   }
 
   private generateLayers() {
-    this.vectorTileLayer = this.getVectortileLayer(new MapProjection(this.tileUrl).Projection);
-    this.setStyle(this.vectorTileLayer);
+    this.vectorTileLayer = this.getVectortileLayer(new MapProjection(this.tileUrl).Projection)
+    this.setStyle(this.vectorTileLayer)
 
 
-    let layers = [this.vectorTileLayer] as BaseLayer[] | Collection<BaseLayer> | LayerGroup | undefined;
+    let layers = [this.vectorTileLayer] as BaseLayer[] | Collection<BaseLayer> | LayerGroup | undefined
 
     if (this.showGrid) {
       const debugLayer = new TileLayer({
@@ -315,10 +314,10 @@ export class
           wrapX: this.vectorTileLayer.getSource()!.getWrapX(),
           zDirection: this.vectorTileLayer.getSource()!.zDirection
         }),
-      });
-      layers = [this.vectorTileLayer, debugLayer];
+      })
+      layers = [this.vectorTileLayer, debugLayer]
     }
-    return layers;
+    return layers
   }
 
   private setStyle(vectorTileLayer: VectorTileLayer) {
@@ -329,10 +328,10 @@ export class
 
           //overrule source url from style
           if (this.tileUrl !== NetherlandsRDNewQuadDefault) {
-            vectorTileLayer.getSource()?.setUrl(this.tileUrl + this.selector);
+            vectorTileLayer.getSource()?.setUrl(this.tileUrl + this.selector)
           }
         })
-        .catch((err) => console.error('error loading: ' + this.id + ' ' + this.styleUrl + ' ' + err));
+        .catch((err) => console.error('error loading: ' + this.id + ' ' + this.styleUrl + ' ' + err))
     }
     else {
       const defaultStyle = new Style({
@@ -345,23 +344,10 @@ export class
         })
       })
       vectorTileLayer.setStyle(defaultStyle)
-
-
     }
   }
 
-  private setNewProjection() {
-    const newLayers = this.generateLayers() as BaseLayer[] | Collection<BaseLayer>;
-    const newView = new View({
-      center: this.map.getView().getCenter(),
-      zoom: this.zoom,
-      enableRotation: false,
-      projection: this.vectorTileLayer.getSource()?.getProjection() as ProjectionLike,
-    });
-    this.map.setView(newView);
-    this.map.setLayers(newLayers);
-    //console.log('project ' + JSON.stringify(this.vectorTileLayer.getSource()?.getProjection()))
-  }
+  
 
   getVectortileLayer(projection: Projection): VectorTileLayer {
     const vectorTileLayer = new VectorTileLayer(
@@ -371,23 +357,21 @@ export class
         declutter: true,
         useInterimTilesOnError: false
 
-      });
-
+      })
     return (vectorTileLayer)
   }
 
   private calcResolutions(projection: Projection) {
-    const tileSizePixels = 256;
-    const tileSizeMtrs = getWidth(projection.getExtent()) / tileSizePixels;
-    let resolutions: Array<number> = [];
+    const tileSizePixels = 256
+    const tileSizeMtrs = getWidth(projection.getExtent()) / tileSizePixels
+    let resolutions: Array<number> = []
     for (let i = 0; i <= 21; i++) {
-      resolutions[i] = tileSizeMtrs / Math.pow(2, i);
+      resolutions[i] = tileSizeMtrs / Math.pow(2, i)
     }
     return (resolutions)
   }
 
   private getVectorTileSource(projection: Projection, url: string) {
-
     return new VectorTileSource({
       format: new MVT(),
       projection: projection,
@@ -397,17 +381,10 @@ export class
     })
   }
 
+  isFullURL(url: string): boolean {
+    return url.toLowerCase().startsWith('http://') || url.toLowerCase().startsWith('https://')
+ }
 
-
-  getMapStyle() {
-    return ` z-index: 1;
-    position: relative;  
-    display: flex;"
-    width: 300px;
-    height: 400px;
-    `
-
-  }
 }
 
 
