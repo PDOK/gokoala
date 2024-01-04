dbdde31c54e4bf91b56012b89fe69337baa29b63
Richard Kettelerij <richard@mindloops.nl>
2024-01-04 12:45:10 +0100

    Refactoring: swap custom merge for Mergo lib

--- engine/util/json_test.go
+++ engine/util/json_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestJSONMerge_identical_json_input_should_not_result_differences(t *testing.T) {
+func TestJSONMerge_identical_json_input_should_not_result_in_differences(t *testing.T) {
 	// given
 	file, err := filepath.Abs("../testdata/ogcapi-tiles-1.bundled.json")
 	if err != nil {
@@ -37,4 +37,42 @@ func TestJSONMerge_identical_json_input_should_not_result_differences(t *testing
 
 	// then
 	assert.JSONEq(t, string(expected), string(actual))
+}
+
+func TestJSONMerge_two_openapi_spec(t *testing.T) {
+	// given
+	fileTiles, err := filepath.Abs("../testdata/ogcapi-tiles-1.bundled.json")
+	if err != nil {
+		t.Fatalf("can't locate testdata %v", err)
+	}
+	fileTilesContent, err := os.ReadFile(fileTiles)
+	if err != nil {
+		t.Fatalf("can't read testdata %v", err)
+	}
+
+	fileFeatures, err := filepath.Abs("../testdata/ogcapi-features-1.resolved.json")
+	if err != nil {
+		t.Fatalf("can't locate testdata %v", err)
+	}
+	fileFeaturesContent, err := os.ReadFile(fileFeatures)
+	if err != nil {
+		t.Fatalf("can't read testdata %v", err)
+	}
+
+	// when
+	actual, err := MergeJSON(fileFeaturesContent, fileTilesContent)
+	if err != nil {
+		t.Fatalf("JSON merge failed %v", err)
+	}
+
+	// then
+	fileExpected, err := filepath.Abs("../testdata/ogcapi-merged.json")
+	if err != nil {
+		t.Fatalf("can't locate testdata %v", err)
+	}
+	fileExpectedContent, err := os.ReadFile(fileExpected)
+	if err != nil {
+		t.Fatalf("can't read testdata %v", err)
+	}
+	assert.JSONEq(t, string(fileExpectedContent), string(actual))
 }
