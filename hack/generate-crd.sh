f103fe2abf18e5cd5972826ea3d9e2590f6181d8
Richard Kettelerij <richard@mindloops.nl>
2024-03-11 16:32:52 +0100

    GoKoala CRD support: Polishing

--- hack/generate-crd.sh
+++ hack/generate-crd.sh
@@ -7,6 +7,13 @@ set -o pipefail
 GOKOALA_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 cd "${GOKOALA_ROOT}"
 
+# Build controller-tools
 docker build -f hack/crd/Dockerfile -t gokoala-controller-tools .
-docker run -v `pwd`/:/gokoala gokoala-controller-tools crd paths="/gokoala/hack/crd/..." output:stdout
+
+# Run against GoKoala
+docker run -v `pwd`/:/gokoala gokoala-controller-tools crd paths="/gokoala/hack/crd/..." output:dir="/gokoala/hack/tmp"
+
+# Assertions
+cat hack/tmp/pdok_gokoalas.yaml
+cat hack/tmp/pdok_gokoalas.yaml | grep "kind: GoKoala"
 
