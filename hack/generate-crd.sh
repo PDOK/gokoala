f05eda0e0ff433fc81e6a8eda507e79c6f815948
Richard Kettelerij <richard@mindloops.nl>
2024-03-12 15:00:46 +0100

    GoKoala CRD support: Also use local or Docker build

--- hack/generate-crd.sh
+++ hack/generate-crd.sh
@@ -7,11 +7,20 @@ set -o pipefail
 GOKOALA_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 cd "${GOKOALA_ROOT}"
 
-# Build controller-tools
-docker build -f hack/crd/Dockerfile -t gokoala-controller-tools .
+if ! command -v controller-gen &> /dev/null
+then
+    echo "controller-gen not found, using Docker container instead"
+    # Build controller-tools
+    docker build -f hack/crd/Dockerfile -t gokoala-controller-tools .
 
-# Run against GoKoala
-docker run -v `pwd`/:/gokoala gokoala-controller-tools crd paths="/gokoala/hack/crd/..." output:dir="/gokoala/hack/tmp"
+    # Run against GoKoala
+    docker run -v `pwd`/:/gokoala gokoala-controller-tools crd paths="/gokoala/hack/crd/..." output:dir="/gokoala/hack/tmp"
+else
+   echo "controller-gen found, using this local install instead of Docker container"
+
+   # Run against GoKoala config
+   controller-gen object paths="$(pwd)/hack/crd/..." output:dir="$(pwd)hack/tmp"
+fi
 
 # Assertions
 cat hack/tmp/pdok_gokoalas.yaml
