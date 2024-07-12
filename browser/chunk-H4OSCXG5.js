d4363eee027f656b25f20ab46be1422a576dd56a
JLSchaap <John.Schaap@Kadaster.nl>
2024-07-12 09:43:20 +0200

    Auto-generated commit [ci skip]

---
+++ browser/chunk-H4OSCXG5.js
@@ -0,0 +1,1 @@
+import{a as e}from"./chunk-PZJJYAMS.js";import{a as r}from"./chunk-ISMGDHAB.js";import"./chunk-5FZOKLP6.js";var o=class extends r{decodeBlock(t){return e(new Uint8Array(t)).buffer}};export{o as default};
