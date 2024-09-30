b6cc092e106361e7781319338999cf2e28fdf1c0
Richard Kettelerij <richard@mindloops.nl>
2024-09-30 13:43:59 +0200

    feat: add test

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -80,6 +80,12 @@ func Test_newRouter(t *testing.T) {
 			apiCall:    "http://localhost:8181/conformance?f=html",
 			wantBody:   "internal/engine/testdata/expected_processes_conformance.html",
 		},
+		{
+			name:       "Should have valid sitemap XML",
+			configFile: "examples/config_all.yaml",
+			apiCall:    "http://localhost:8181/sitemap.xml",
+			wantBody:   "internal/engine/testdata/expected_sitemap.xml",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -107,7 +113,7 @@ func Test_newRouter(t *testing.T) {
 			switch {
 			case strings.HasSuffix(tt.apiCall, "json"):
 				assert.JSONEq(t, recorder.Body.String(), string(expectedBody))
-			case strings.HasSuffix(tt.apiCall, "html"):
+			case strings.HasSuffix(tt.apiCall, "html") || strings.HasSuffix(tt.apiCall, "xml"):
 				assert.Contains(t, normalize(recorder.Body.String()), normalize(string(expectedBody)))
 			default:
 				log.Fatalf("implement support to test format: %s", tt.apiCall)
