3466fe636a25bd85d4ef78f1f7f76748a354eb55
Richard Kettelerij <richard@mindloops.nl>
2024-10-19 20:41:45 +0200

    chore(docs): fix test

--- hack/generate-config-docs.sh
+++ hack/generate-config-docs.sh
@@ -9,11 +9,12 @@ cd "${GOKOALA_ROOT}"
 
 if ! command -v type2md &> /dev/null
 then
-  echo "installing type2md"
-  # using fork: https://github.com/rkettelerij/type2md/tree/fork maybe permanently but at least
-  # until PRs https://github.com/eleztian/type2md/pull/1 and https://github.com/eleztian/type2md/pull/2 are merged
-  go get github.com/rkettelerij/type2md@c111a3b53690f56f658c742bda79448ed0b398e6
-  go install github.com/rkettelerij/type2md@c111a3b53690f56f658c742bda79448ed0b398e6
+  echo "installing type2md tool"
+  # using fork: https://github.com/rkettelerij/type2md/tree/fork, likely permanently but at least
+  # until the following PRs are merged.
+  # - https://github.com/eleztian/type2md/pull/1
+  # - https://github.com/eleztian/type2md/pull/2
+  go install -mod readonly github.com/rkettelerij/type2md@c111a3b53690f56f658c742bda79448ed0b398e6
 fi
 
 echo "generating markdown configuration reference from config structs"
