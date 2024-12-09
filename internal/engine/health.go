ea97c20493875b491173d2c8a021db220f6f8107
Michiel Korpel <michiel.korpel@kadaster.nl>
2024-12-09 17:23:41 +0100

    feat(health): expanded health check when OGC API Tiles configured

--- internal/engine/health.go
+++ internal/engine/health.go
@@ -2,10 +2,28 @@ package engine
 
 import (
 	"net/http"
+	"net/url"
+	"time"
 )
 
 func newHealthEndpoint(e *Engine) {
-	e.Router.Get("/health", func(w http.ResponseWriter, _ *http.Request) {
-		SafeWrite(w.Write, []byte("OK"))
-	})
+	if tilesConfig := e.Config.OgcAPI.Tiles; tilesConfig != nil {
+		client := &http.Client{Timeout: time.Duration(500) * time.Millisecond}
+		target, _ := url.Parse(tilesConfig.DatasetTiles.TileServer.String() + *tilesConfig.DatasetTiles.HealthCheck.TilePath)
+
+		e.Router.Get("/health", func(w http.ResponseWriter, _ *http.Request) {
+			resp, err := client.Head(target.String())
+			if err != nil {
+				// exact error is irrelevant for health monitoring
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(resp.StatusCode)
+				resp.Body.Close()
+			}
+		})
+	} else {
+		e.Router.Get("/health", func(w http.ResponseWriter, _ *http.Request) {
+			SafeWrite(w.Write, []byte("OK"))
+		})
+	}
 }
