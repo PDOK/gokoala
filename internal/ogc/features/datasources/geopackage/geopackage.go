80a00c7f845bf43a16424dd729b84be5c7628e85
Richard Kettelerij <richard@mindloops.nl>
2025-01-03 15:47:20 +0100

    chore: enable more linters (+ fix some issues)

--- internal/ogc/features/datasources/geopackage/geopackage.go
+++ internal/ogc/features/datasources/geopackage/geopackage.go
@@ -25,8 +25,6 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/mattn/go-sqlite3"
 	"github.com/qustavo/sqlhooks/v2"
-
-	_ "github.com/mattn/go-sqlite3" // import for side effect (= sqlite3 driver) only
 )
 
 const (
