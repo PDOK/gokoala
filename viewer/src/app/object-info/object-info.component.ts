896da07dec1caad2944e1ab7c54d2b096dfc982a
john.schaap@kadaster.nl <John.Schaap@Kadaster.nl>
2025-02-25 13:51:23 +0100

    draw location search box

--- viewer/src/app/object-info/object-info.component.ts
+++ viewer/src/app/object-info/object-info.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import RenderFeature from 'ol/render/Feature'
+import { WKT } from 'ol/format'
 
 type propRow = {
   title: string
@@ -25,10 +26,18 @@ export class ObjectInfoComponent {
 
       for (const val in prop) {
         if (val !== 'mapbox-layer') {
-          const p: propRow = { title: val, value: prop[val] }
-          propTable.push(p)
+          if (val === 'geometry') {
+            const wktFormat = new WKT()
+            const wktString = wktFormat.writeGeometry(prop[val])
+            const geop: propRow = { title: val, value: wktString }
+            propTable.push(geop)
+          } else {
+            const p: propRow = { title: val, value: prop[val] }
+            propTable.push(p)
+          }
         }
       }
+
       return propTable
     } else {
       return []
