0504ff9121b7ac326d80221f049c4f0e0e4ca4dc
john.schaap@kadaster.nl <John.Schaap@Kadaster.nl>
2025-04-11 16:02:42 +0200

    code cleanup

--- viewer/src/environments/environment.development.ts
+++ viewer/src/environments/environment.development.ts
@@ -1,8 +1,7 @@
 import { NgxLoggerLevel } from 'ngx-logger'
-import { initialCurrentHttp } from 'src/app/app.module'
 
 export const environment = {
   bgtBackgroundUrl: 'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0?',
-  loglevel: NgxLoggerLevel.DEBUG,
-  currentHttp: initialCurrentHttp,
+  loglevel: NgxLoggerLevel.DEBUG
+
 }
