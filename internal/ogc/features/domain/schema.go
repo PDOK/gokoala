e967842e5b8f544b218c00915ef730cf54039c23
Richard Kettelerij <richard@mindloops.nl>
2025-05-08 15:50:56 +0200

    feat: implement OAF part 5 schemas: comments and typo's

--- internal/ogc/features/domain/schema.go
+++ internal/ogc/features/domain/schema.go
@@ -35,21 +35,33 @@ func (f Field) ToJSONSchemaTypeFormat() TypeFormat {
 		return TypeFormat{Type: "boolean"}
 	case "text", "char", "character", "character varying", "varchar", "nvarchar", "clob":
 		return TypeFormat{Type: "string"}
-	case "uuid":
-		return TypeFormat{Type: "string", Format: "uuid"}
-	case "date":
-		return TypeFormat{Type: "string", Format: "date"}
-	case "time":
-		return TypeFormat{Type: "string", Format: "time"}
-	case "datetime", "timestamp":
-		return TypeFormat{Type: "string", Format: "date-time"}
-	case "real", "float", "double", "double precision", "numeric", "decimal":
-		return TypeFormat{Type: "number"}
 	case "int", "integer", "tinyint", "smallint", "mediumint", "bigint", "int2", "int8":
 		return TypeFormat{Type: "integer"}
+	case "real", "float":
+		return TypeFormat{Type: "number", Format: "float"}
+	case "double", "double precision", "numeric", "decimal":
+		return TypeFormat{Type: "number", Format: "double"}
+	case "uuid":
+		// From OAF Part 5: Properties that represent a UUID SHOULD be represented as a string with format "uuid".
+		return TypeFormat{Type: "string", Format: "uuid"}
+	case "date":
+		// From OAF Part 5: Each temporal property SHALL be a "string" literal with the appropriate format
+		// (e.g., "date-time" or "date" for instances, depending on the temporal granularity).
+		return TypeFormat{Type: "string", Format: "date"}
+	case "time":
+		// From OAF Part 5: Each temporal property SHALL be a "string" literal with the appropriate format
+		// (e.g., "date-time" or "date" for instances, depending on the temporal granularity).
+		return TypeFormat{Type: "string", Format: "time"}
+	case "datetime", "timestamp":
+		// From OAF Part 5: Each temporal property SHALL be a "string" literal with the appropriate format
+		// (e.g., "date-time" or "date" for instances, depending on the temporal granularity).
+		return TypeFormat{Type: "string", Format: "date-time"}
 	case "geometry", "geometrycollection":
+		// From OAF Part 5: the following special value is supported: "geometry-any" as the wildcard for any geometry type
 		return TypeFormat{Type: lowerCaseType, Format: "geometry-any"}
 	case "point", "linestring", "polygon", "multipoint", "multilinestring", "multipolygon":
+		// From OAF Part 5: Each spatial property SHALL include a "format" member with a string value "geometry",
+		// followed by a hyphen, followed by the name of the geometry type in lower case
 		return TypeFormat{Type: lowerCaseType, Format: "geometry-" + lowerCaseType}
 	default:
 		return TypeFormat{Type: lowerCaseType}
