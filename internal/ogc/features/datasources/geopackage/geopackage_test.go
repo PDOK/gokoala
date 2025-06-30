4a49a335bdccefbf7862303a0c340808b5d8ec3b
Richard Kettelerij <richard@mindloops.nl>
2025-06-30 17:01:32 +0200

    feat(datasource): add Postgres support. Introduce datasource config capable of on-the-fly reprojection/transformation

--- internal/ogc/features/datasources/geopackage/geopackage_test.go
+++ internal/ogc/features/datasources/geopackage/geopackage_test.go
@@ -73,7 +73,7 @@ func TestNewGeoPackage(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			g, err := NewGeoPackage(tt.args.collection, tt.args.config)
+			g, err := NewGeoPackage(tt.args.collection, tt.args.config, false)
 			assert.NoError(t, err)
 			assert.Equalf(t, tt.wantNrOfFeatureTablesInGpkg, len(g.featureTableByCollectionID), "NewGeoPackage(%v)", tt.args.config)
 		})
