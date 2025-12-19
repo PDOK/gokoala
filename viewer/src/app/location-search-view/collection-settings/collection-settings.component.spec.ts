7500566455997d2281f2f5dab579a33c31d8dfb0
Morten Zwarenstein <Morten.Zwarenstein@kadaster.nl>
2025-12-19 11:09:05 +0100

    linting: fixed new linting for rxjs

--- viewer/src/app/location-search-view/collection-settings/collection-settings.component.spec.ts
+++ viewer/src/app/location-search-view/collection-settings/collection-settings.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { CollectionSettingsComponent } from './collection-settings.component';
+import { CollectionSettingsComponent } from './collection-settings.component'
 
 describe('CollectionSettingsComponent', () => {
-  let component: CollectionSettingsComponent;
-  let fixture: ComponentFixture<CollectionSettingsComponent>;
+  let component: CollectionSettingsComponent
+  let fixture: ComponentFixture<CollectionSettingsComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CollectionSettingsComponent]
-    })
-    .compileComponents();
+      imports: [CollectionSettingsComponent],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(CollectionSettingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(CollectionSettingsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
