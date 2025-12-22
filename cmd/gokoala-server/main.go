d2b811c0d23041929b9f8355d344dea56886df80
Richard Kettelerij <richard@mindloops.nl>
2025-12-22 12:59:58 +0100

    chore: use streaming snake func to define env vars, aligning with gokoala-etl

--- cmd/gokoala-server/main.go
+++ cmd/gokoala-server/main.go
@@ -9,70 +9,83 @@ import (
 	"github.com/PDOK/gokoala/config"
 	eng "github.com/PDOK/gokoala/internal/engine"
 	"github.com/PDOK/gokoala/internal/ogc"
+	"github.com/iancoleman/strcase"
 	"github.com/urfave/cli/v2"
+)
+
+const (
+	hostFlag                = "host"
+	portFlag                = "port"
+	debugPortFlag           = "debug-port"
+	shutdownDelayFlag       = "shutdown-delay"
+	configFileFlag          = "config-file"
+	openAPIFileFlag         = "openapi-file"
+	enableTrailingSlashFlag = "enable-trailing-slash"
+	enableCorsFlag          = "enable-cors"
+	themeFileFlag           = "theme-file"
 )
 
 var (
 	cliFlags = []cli.Flag{
 		&cli.StringFlag{
-			Name:     "host",
+			Name:     hostFlag,
 			Usage:    "bind host for OGC server",
 			Value:    "0.0.0.0",
 			Required: false,
-			EnvVars:  []string{"HOST"},
+			EnvVars:  []string{strcase.ToScreamingSnake(hostFlag)},
 		},
 		&cli.IntFlag{
-			Name:     "port",
+			Name:     portFlag,
 			Usage:    "bind port for OGC server",
 			Value:    8080,
 			Required: false,
-			EnvVars:  []string{"PORT"},
+			EnvVars:  []string{strcase.ToScreamingSnake(portFlag)},
 		},
 		&cli.IntFlag{
-			Name:     "debug-port",
+			Name:     debugPortFlag,
 			Usage:    "bind port for debug server (disabled by default), do not expose this port publicly",
 			Value:    -1,
 			Required: false,
-			EnvVars:  []string{"DEBUG_PORT"},
+			EnvVars:  []string{strcase.ToScreamingSnake(debugPortFlag)},
 		},
 		&cli.IntFlag{
-			Name:     "shutdown-delay",
+			Name:     shutdownDelayFlag,
 			Usage:    "delay (in seconds) before initiating graceful shutdown (e.g. useful in k8s to allow ingress controller to update their endpoints list)",
 			Value:    0,
 			Required: false,
-			EnvVars:  []string{"SHUTDOWN_DELAY"},
+			EnvVars:  []string{strcase.ToScreamingSnake(shutdownDelayFlag)},
 		},
 		&cli.StringFlag{
-			Name:     "config-file",
+			Name:     configFileFlag,
 			Usage:    "reference to YAML configuration file",
 			Required: true,
-			EnvVars:  []string{"CONFIG_FILE"},
+			EnvVars:  []string{strcase.ToScreamingSnake(configFileFlag)},
 		},
 		&cli.StringFlag{
-			Name:     "openapi-file",
+			Name:     openAPIFileFlag,
 			Usage:    "reference to a (customized) OGC OpenAPI spec for the dynamic parts of your OGC API",
 			Required: false,
-			EnvVars:  []string{"OPENAPI_FILE"},
+			EnvVars:  []string{strcase.ToScreamingSnake(openAPIFileFlag)},
 		},
 		&cli.BoolFlag{
-			Name:     "enable-trailing-slash",
+			Name:     enableTrailingSlashFlag,
 			Usage:    "allow API calls to URLs with a trailing slash.",
 			Value:    false, // to satisfy https://gitdocumentatie.logius.nl/publicatie/api/adr/#api-48
 			Required: false,
-			EnvVars:  []string{"ALLOW_TRAILING_SLASH"},
+			EnvVars:  []string{strcase.ToScreamingSnake(enableTrailingSlashFlag)},
 		},
 		&cli.BoolFlag{
-			Name:     "enable-cors",
+			Name:     enableCorsFlag,
 			Usage:    "enable Cross-Origin Resource Sharing (CORS) as required by OGC API specs. Disable if you handle CORS elsewhere.",
 			Value:    false,
 			Required: false,
-			EnvVars:  []string{"ENABLE_CORS"},
+			EnvVars:  []string{strcase.ToScreamingSnake(enableCorsFlag)},
 		},
 		&cli.StringFlag{
-			Name:     "theme-file",
+			Name:     themeFileFlag,
 			Usage:    "reference to a (customized) YAML configuration file for the theme",
 			Required: false,
-			EnvVars:  []string{"THEME_FILE"},
+			EnvVars:  []string{strcase.ToScreamingSnake(themeFileFlag)},
 		},
 	}
 )
