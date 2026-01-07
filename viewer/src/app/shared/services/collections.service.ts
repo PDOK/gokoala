7eb7a6a09bbd4ea3f51349f5d51acae0604b1da3
Richard Kettelerij <richard@mindloops.nl>
2026-01-07 17:03:33 +0100

    feat: merge gomagpie - change viewer to work with updated gokoala search API

--- viewer/src/app/shared/services/collections.service.ts
+++ viewer/src/app/shared/services/collections.service.ts
@@ -1,14 +1,12 @@
 import { inject, Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { map, Observable } from 'rxjs'
-import { environment } from '../../../environments/environment'
 import { Link } from '../../link'
 
 export interface Collection {
   id: string
   title: string
   version: number
-  displayNameTemplate: string
   links: Link[]
 }
 
@@ -24,7 +22,7 @@ export class CollectionsService {
   private _http = inject(HttpClient)
 
   getCollections(): Observable<Collection[]> {
-    const url = new URL('collections', environment.locationApi)
+    const url = new URL('collections', window.location.origin)
     url.searchParams.append('f', 'json')
     return this._http.get<CollectionResponse>(url.toString()).pipe(map(res => res.collections))
   }
--- viewer/src/environments/environment.development.ts
+++ viewer/src/environments/environment.development.ts
@@ -2,6 +2,5 @@ import { NgxLoggerLevel } from 'ngx-logger'
 
 export const environment = {
   bgtBackgroundUrl: 'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0?',
-  locationApi: 'https://api.pdok.nl/bzk/location-api/autocomplete/v1-preprod/',
   loglevel: NgxLoggerLevel.DEBUG,
 }
