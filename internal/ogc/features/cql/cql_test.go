bf2e364c532500b8bb56f75e6f94ebfbb91500c9
Richard Kettelerij <richard@mindloops.nl>
2026-01-30 22:05:37 +0100

    test: add extra test for spatial predicate

--- internal/ogc/features/cql/cql_test.go
+++ internal/ogc/features/cql/cql_test.go
@@ -22,6 +22,13 @@ func TestParseToSQL(t *testing.T) {
 				tree: "(cqlFilter (booleanExpression (booleanTerm (booleanFactor (booleanPrimary (predicate (comparisonPredicate (binaryComparisonPredicate (scalarExpression (propertyName amount)) <= (scalarExpression (numericLiteral 100))))))))) <EOF>)",
 			},
 		},
+		{
+			name: "spatial",
+			args: args{
+				cql:  "S_INTERSECTS(geometry,POINT(36.319836 32.288087))",
+				tree: "(cqlFilter (booleanExpression (booleanTerm (booleanFactor (booleanPrimary (predicate (spatialPredicate S_INTERSECTS ( (geomExpression (propertyName geometry)) , (geomExpression (spatialInstance (geometryLiteral (point POINT ( (coordinate (xCoord 36.319836) (yCoord 32.288087)) ))))) ))))))) <EOF>)",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
