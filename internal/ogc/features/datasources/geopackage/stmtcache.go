6d81a37611705ea8d384e572b5450b91732efcd8
Richard Kettelerij <richard@mindloops.nl>
2026-02-13 16:46:51 +0100

    chore: reduce to 50 for now.

--- internal/ogc/features/datasources/geopackage/stmtcache.go
+++ internal/ogc/features/datasources/geopackage/stmtcache.go
@@ -8,7 +8,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-var preparedStmtCacheSize = 100
+var preparedStmtCacheSize = 50
 
 // PreparedStatementCache is thread safe.
 type PreparedStatementCache struct {
